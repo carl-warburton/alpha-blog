6e7aadbccba6b47ffbfe86446e3d4cb772d84793
carl-warburton <carl.warburton1992@gmail.com>
2018-01-05 13:28:23 +1100

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 
   def index
     @articles = Article.all
@@ -54,6 +56,10 @@ class ArticlesController < ApplicationController
 
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
